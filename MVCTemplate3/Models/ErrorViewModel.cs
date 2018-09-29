eb49c8b468fee154ec8649e62fc26ccd19a7941c
sidrazaheer <sidra.zaheer1@gmail.com>
2018-09-29 17:08:53 -0400

    Add project files.

---
+++ MVCTemplate3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCTemplate3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
